a6f23e5d47576239220a31460808a17e97feee57
Dahyun Kwon <142938702+dhk486@users.noreply.github.com>
2025-06-05 14:25:38 -1000

    Basic Enemy Movement
    Implement enemy direction change at cliff or all using Raycast2D

--- catastrophe(draft)/Nodes/Enemies/enemy_1.gd
+++ catastrophe(draft)/Nodes/Enemies/enemy_1.gd
@@ -1,20 +1,39 @@
 extends Node2D
+
+
 class_name Enemy 
 
 
 # variables
+const SPEED = 30
+var direction = -1
+
 @onready var health = 10
-
-
+@onready var ray_cast_right = $RayCastRight
+@onready var ray_cast_left = $RayCastLeft
+@onready var ray_cast_bottomL = $RayCastBottomLeft
+@onready var ray_cast_bottomR = $RayCastBottomRight
+@onready var animated_sprite = $AnimatedSprite2D
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+#func _ready() -> void:
+	#pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+func _process(delta):
+	# if enemy detects  a wall, change its direction
+	if ray_cast_right.is_colliding():
+		direction = -1
+	elif ray_cast_left.is_colliding():
+		direction = 1
+	# if enemy detects cliff, change its direction
+	elif  direction == 1 and !ray_cast_bottomR.is_colliding():
+		direction = -1
+	elif direction == -1 and !ray_cast_bottomL.is_colliding():
+		direction = 1
+	animated_sprite.flip_h = direction == -1
+	
+	position.x += direction * SPEED * delta	
 
 # Attack area for player
 func _on_attack_area_body_entered(body: Node2D) -> void:
